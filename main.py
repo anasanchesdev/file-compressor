1ca785e37dd36032043512be7845b2c30a39c410
Dev. Sanches <dev.anaclarasanches@gmail.com>
2024-01-29 11:37:26 -0300

    Front-End for unzip.

--- main.py
+++ main.py
@@ -4,35 +4,65 @@ import PySimpleGUI as ps
 FONT = ('Segoe UI Black', 10)
 ACTION_FONT = ('Segoe UI', 10)
 
-print(ps.Text.fonts_installed_list())
 ps.theme('Dark')
 title = ps.Text('FILE COMPRESSOR', font=('Copperplate Gothic Bold', 20), text_color='green')
 dev_credits = ps.Text('by: anasanchesdev', font=('Courier', 8), text_color='lime green', pad=((6, 0), (0, 15)))
-label1 = ps.Text('Select files to compress: ', font=FONT)
-label2 = ps.Text('Select destination folder: ', font=FONT, pad=((2, 0), (5, 0)))
-label3 = ps.Text('Type in the zip file name: ', font=FONT, pad=((5, 0), (40, 0)))
+z_file_label = ps.Text('Select files: ', font=FONT)
+z_folder_label = ps.Text('Select destination folder: ', font=FONT, pad=((2, 0), (5, 0)))
+z_name_label = ps.Text('Type in the zip file name: ', font=FONT, pad=((5, 0), (40, 0)))
+zip_slider = ps.Text('ZIP')
+unzip_slider = ps.Text('UNZIP')
+slider = ps.Slider(key='slider', range=(0, 1), disable_number_display=True, enable_events=True, orientation='h',
+                   size=(5, 5))
 action_text = ps.Text('', key='action_text', font=ACTION_FONT)
 
-input_box1 = ps.InputText(key='files_input')
-input_box2 = ps.InputText(key='folder_input')
-input_box3 = ps.InputText(key='zip_name')
+z_file_input = ps.InputText(key='files_input')
+z_folder_input = ps.InputText(key='folder_input')
+z_name_input = ps.InputText(key='zip_name')
 
-button1 = ps.FilesBrowse('Choose', key='files_button')
-button2 = ps.FolderBrowse('Choose', key='folder_button')
-button3 = ps.Button('Compress', key='compress_button')
+z_file_button = ps.FilesBrowse('Choose', key='files_button')
+z_folder_button = ps.FolderBrowse('Choose', key='folder_button')
+z_action_button = ps.Button('Compress', key='compress_button')
 
-# ui format
-window = ps.Window('File compressor', layout=[
+uz_file_label = ps.Text('Select files to compress: ', font=FONT)
+uz_folder_label = ps.Text('Select destination folder: ', font=FONT, pad=((2, 0), (5, 0)))
+uz_action_text = ps.Text('', key='uz_action_text', font=ACTION_FONT)
+
+uz_files_input = ps.InputText(key='uz_files_input')
+uz_folder_input = ps.InputText(key='uz_folder_input')
+
+uz_file_button = ps.FileBrowse('Choose', key='uz_files_button')
+uz_folder_button = ps.FolderBrowse('Choose', key='uz_folder_button')
+uz_action_button = ps.Button('Compress', key='decompress_button')
+
+zip_layout = [
+    [z_file_label],
+    [z_file_input, z_file_button],
+    [z_folder_label],
+    [z_folder_input, z_folder_button],
+    [z_name_label],
+    [z_name_input, z_action_button],
+    [action_text]
+]
+
+unzip_layout = [
+    [uz_file_label],
+    [uz_files_input, uz_file_button],
+    [uz_folder_label],
+    [uz_folder_input, uz_folder_button],
+]
+
+layout = [
     [title],
     [dev_credits],
-    [label1],
-    [input_box1, button1],
-    [label2],
-    [input_box2, button2],
-    [label3],
-    [input_box3, button3],
-    [action_text]
-])
+    [zip_slider, slider, unzip_slider],
+    [ps.Column(zip_layout, key='zip')],
+    [ps.Column(unzip_layout, key='unzip', visible=False)]
+]
+
+# ui format
+window = ps.Window('File compressor', layout=layout)
+
 
 while True:
 
